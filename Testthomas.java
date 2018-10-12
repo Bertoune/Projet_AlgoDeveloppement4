b8020c3ba17f7b1ef093e4f03a9d8bc41c16a54e
Bertoune <43672849+Bertoune@users.noreply.github.com>
2018-10-12 13:42:10 +0200

    Delete Testthomas.java

--- Testthomas.java
+++
@@ -1,9 +0,0 @@
-package projectClass;
-
-public class Testthomas {
-	//ceci est un test
-	// je voudrais voir si la modification s'effectue direcement sur le site !! 
-	//test
-	//je vais essayer
-	//voici la modification 1.2
-}
