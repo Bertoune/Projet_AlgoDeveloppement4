06d876070b847a2a60644b5fa336173268e96584
Bertoune <43672849+Bertoune@users.noreply.github.com>
2018-10-12 15:41:36 +0200

    METHODE FONCTIONNELLE QUI CHERCHE ET TROUVE DES MO

--- motDuDictionnaire.java
+++ motDuDictionnaire.java
@@ -1,4 +1,5 @@
-import java.io.File; 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader; 
@@ -7,22 +8,58 @@ public class motDuDictionnaire {
 
 	// on va chercher le chemin du fichier "Dictionnaire.txt" afin d'analyser si le mot saisit par l'utilsateur est bien present
 	
-	public boolean rechercherMot() throws IOException {
+	public static boolean appartientMot(String unMot) throws FileNotFoundException {
 		
-	motDuDictionnaire mot= new motDuDictionnaire(); // saisir
+		File f = new File("Dictionnaire.txt");
+		FileReader fr = new FileReader (f);
+		// BufferedReader permet de lire ligne par ligne un fichier
+		// c'est utile car ici 1 mot = 1 ligne
 		
-	File f = new File("C:/flux.txt"); 
-
-	String line = null;
-
-	while ((line = reader.readLine()) != null) 
-		{ 
-			if (line.contains(mot)) 
-				{ 
+		BufferedReader br = new BufferedReader (fr);
+		
+		
+		try 
+		{
+		String line = br.readLine();
+		
+		while (line!= null)
+		{
+			//Tant que le fichier n'est pas parcouru
+		
+			// c'est a dire tant qu'il y a des mots
+			
+			if (line.equals(unMot))
+				{
+					
+					line= null;
 					return true;
-				} 
-			else return false;
-		} 
-	} 
+				}
+			
+			line=br.readLine();
+			
+				
+		}
+		
+	
+		
+		br.close();
+		fr.close();
+	}
+		 catch (IOException exception)
+		    {
+		        System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
+		    }
+		return false;
+	}
+	
+	public static void main(String []args) throws FileNotFoundException
+	{
+		String mot="TAMERE";
+		if (appartientMot(mot))
+		{
+			System.out.println("Trouve");
+		}
+		else System.out.println("Nope");
+	}
 	
 }
