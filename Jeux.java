37984b73ef36969d97435fbe399451735d3b8d74
harivelo <43673103+harivelo@users.noreply.github.com>
2018-10-12 15:02:43 +0200

    Add files via upload

---
+++ Jeux.java
@@ -0,0 +1,29 @@
+package packageClasse;
+
+public class Jeux {
+	private String typeJeux;
+	private String regle;
+	private Grille grille;
+	public Jeux() {
+		
+	}
+	public void afficherGrille( Grille g)
+	{}
+	public boolean jouer()
+	{
+		return true;
+	}
+	public boolean annulerJeux(Grille g)
+	{
+		return true;
+	}
+	public boolean poseJeux(Jeux monjeux, Joueur joueur) 
+	{
+		return true;
+	}
+	public boolean reprendreJeux(Jeux monJeux, Joueur joueur)
+	{
+		return true;
+	}
+	
+}
